c8b300e603260e20102a27a54d3fdd04a9a497a9
nuno.mendonca <nmiguelmendonca@gmail.com>
2020-10-12 12:20:43 +0100

    initial commit

---
+++ resources/js/components/AppContainer.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app-bar
+      class="navbar"
+      app
+      flat>
+
+      <div class="d-flex align-center">
+          <v-img
+            lazy-src="https://picsum.photos/id/11/10/6"
+            class="shrink mr-2 logo"
+            contain
+            transition="scale-transition"
+            width="40"
+            src="https://picsum.photos/id/11/500/300"
+          ></v-img>
+
+          <label
+              class="app-name">
+              SPMS
+          </label>
+      </div>
+
+      <v-spacer></v-spacer>
+      
+      
+      <v-btn text>
+          Nuno Mendonça
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+      
+
+      <v-btn
+          class="mx-3"
+          icon>
+          <v-icon size="24px">fas fa-search</v-icon>
+      </v-btn>
+
+      <v-btn
+          class="mx-3"
+          icon>
+          <v-icon size="24px">fas fa-lock</v-icon>
+      </v-btn>
+  </v-app-bar>
+</template>
+
+
+
+<script>
+  export default {
+    //
+  }
+</script>
---
+++ resources/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+Vue.use(Vuetify)
+
+const opts = {
+    icons: {
+        iconfont: 'fa'
+    }
+}
+
+export default new Vuetify({
+    icons: {
+      iconfont: 'fa'
+    },
+  })
